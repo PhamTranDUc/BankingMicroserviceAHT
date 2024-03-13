2938d604e0534a395da6be2a1a949a983a282526
hungkhmt <hung11812@gmail.com>
2024-03-13 13:55:53 +0700

    Success service 1

---
+++ Hungnt/UserManagementService/src/main/java/com/aht/UserManagementService/Validation/User/UsernameNotExistsValidator.java
@@ -0,0 +1,21 @@
+package com.aht.UserManagementService.Validation.User;
+
+import com.aht.UserManagementService.service.IUserService;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+
+public class UsernameNotExistsValidator implements ConstraintValidator<UsernameNotExists, String> {
+    @Autowired
+    IUserService userService;
+
+    public boolean isValid(String username, ConstraintValidatorContext constraintValidatorContext) {
+
+        if (StringUtils.isEmpty(username)) {
+            return true;
+        }
+
+        return !userService.isUserExistsByUsername(username);
+    }
+}
