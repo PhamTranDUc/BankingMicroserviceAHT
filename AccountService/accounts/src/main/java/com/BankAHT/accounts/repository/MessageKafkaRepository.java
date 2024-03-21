c4c2259f9d673713d9625da7bed7d4b29b0ec362
huy nguyen <huynn@hivetech.vn>
2024-03-21 15:54:09 +0700

    fix project structure

--- AccountService/accounts/src/main/java/com/BankAHT/accounts/repository/MessageKafkaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.BankAHT.accounts.repository;
-
-import com.BankAHT.accounts.entity.MessageKafka;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MessageKafkaRepository extends JpaRepository<MessageKafka,Long> {
-}
